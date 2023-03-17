18292ec6c9fc19550ba46ae092ce8687c336b472
root <root@DESKTOP-NQ3E1IC.localdomain>
2023-03-17 19:47:25 +0800

    rewritten code to use loop

--- src/even.js
+++ src/even.js
@@ -2,42 +2,53 @@ import readlineSync from 'readline-sync';
 
 export default () => {
 
-const number = Math.floor(Math.random() * 101);
+let number = Math.floor(Math.random() * 101);
 let score = 0;
-const name = '';
-const answerArr = [yes, no];
+let name = '';
+const answerArr = ['yes', 'no'];
 
 const greetings = () => {
     console.log('Welcome to the Brain Games!');
-	const name = readlineSync.question('May I have your name? ');
+	name = readlineSync.question('May I have your name? ');
 	console.log(`Hello ${name}`);
     console.log('Answer "yes" if the number is even, otherwise answer "no".');
 
 }
-
-const question = () => readlineSync.question(`Question ${number}`);
 
 const correctMessage = () => {
     console.log('Correct!');
     score += 1;
   };
 
-const wrongMessage = () => {
-    if ((number % 2 === 0) || (question === answerArr[1])) {
+
+
+let i = 0;
+greetings();
+
+for (let i = 1 ; i<= 3; i +=1) {
+    let number = Math.floor(Math.random() * 101);
+    
+    const question = readlineSync.question(`Question ${number}\n`);
+    if ((number % 2 === 0) && (question === answerArr[1])) {
+        
         console.log(`${question} is wrong answer ;(. Correct answer was ${answerArr[0]}.\nLet's try again, ${name}!`);
+        return;
+        
     }
-    else if ((number % 2 !== 0) || (question === answerArr[0])) {
+    else if ((number % 2 !== 0) && (question === answerArr[0])) {
+        
         console.log(`${question} is wrong answer ;(. Correct answer was ${answerArr[1]}.\nLet's try again, ${name}!`);
+        return;
+        
     }
-    score = 0;
+    else {
+        correctMessage();
+    }
+    
+    
 }
 
-
-
-const gameRound = () => {
-    greetings();
-    question();
+return console.log(`Congratulations, ${name}`);
 
 }
 
-}
