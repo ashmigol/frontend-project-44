39a2370d95b54873cddbedeeecc2a9325b49dfac
root <root@DESKTOP-NQ3E1IC.localdomain>
2023-10-18 13:55:39 +0000

    final changes

--- src/games/prime.js
+++ src/games/prime.js
@@ -7,8 +7,8 @@ const isPrime = (num) => {
   if (num <= 1) {
     return false;
   }
-  for (let i = 2; i <= Math.sqrt(n); i += 1) {
-    if (n % i === 0) {
+  for (let i = 2; i <= Math.sqrt(num); i += 1) {
+    if (num % i === 0) {
       return false;
     }
   }
